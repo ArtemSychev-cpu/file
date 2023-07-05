565cc77e2c15c23ba9f1b9b86fbbf3b7d4634333
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-07-05 18:57:02 +0300

    Create User.h

---
+++ User.h
@@ -0,0 +1,28 @@
+#pragma once
+class User
+{
+   string _name;
+   string _login;
+   string _pass;
+
+public:
+	User(string name, string login, string pass) :_name(name), _login(login), _pass(pass) {}
+
+	friend fstream& operator >>(fstream& is, User& obj)
+	{
+		is >> obj._name;
+		is >> obj._login;
+		is >> obj._pass;
+		return is;
+	}
+	friend ostream& operator <<(ostream& os, const User& obj)
+	{
+		os << obj._name;
+		os << ' ';
+		os << obj._login;
+		os << ' ';
+		os << obj._pass;
+		return os;
+	}
+
+};
